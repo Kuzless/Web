a36c63e48ce835588f30b69abeeb9688436b6607
Kuzless <deadgoatbro@gmail.com>
2023-02-07 23:26:50 +0200

    upd1

--- Liptons_Landing/js/header.js
+++ Liptons_Landing/js/header.js
@@ -1,6 +1,9 @@
 window.onscroll = function() {myFunction()};
 var header = document.getElementById("header");
 var sticky = header.offsetTop;
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
 function myFunction() {
     if (window.pageYOffset > sticky) {
       header.classList.add("sticky");
